40171b1bd607bf30984b4e2acc63967f83edaf03
Maggots <gonda.akosdonat@gmail.com>
2025-01-23 16:56:46 +0100

    Script doctoring
    better styled js code, work of me

--- js.js
+++ js.js
@@ -7,105 +7,84 @@ var gazar;
 var vizar;
 var kozosar;
 
-function szamol() {
+function szamol()
+{
+    aram = parseFloat(document.getElementById("aram").value);
+    gaz= parseFloat(document.getElementById("gaz").value);
+    viz= parseFloat(document.getElementById("viz").value);
+    kozos= parseInt(document.getElementById("koz").value);
     
-   aram = parseFloat(document.getElementById("aram").value);
-   gaz= parseFloat(document.getElementById("gaz").value);
-   viz= parseFloat(document.getElementById("viz").value);
-   kozos= parseInt(document.getElementById("koz").value);
-    
-    if(aram<0||aram>25000&&aram!=""){
-        
-        document.getElementById("hiba1").innerHTML="Hiba"
-        document.getElementById("aram").value=""
-        
-        
-    }
-    else{
-        if((aram*12)<1320&&aram!=""){
-            console.log(aram);
-            aramar=aram*14.2;
-            console.log(aramar);
-
-            document.getElementById("aramar").value=aramar;
-
+    if(aram<0||aram>25000&&aram!="")
+        {
+            document.getElementById("hiba1").innerHTML="Hiba"
+            document.getElementById("aram").value=""   
         }
-        else if((aram*12)>1320&&aram!=""){
-            
-            aramar=110*14.2+(aram-110)*15.08;
-            document.getElementById("aramar").value=aramar;
-
-        }
+    else
+    {
+        if((aram*12)<1320&&aram!="")
+            {
+                console.log(aram);
+                aramar=aram*14.2;
+                console.log(aramar);
+                document.getElementById("aramar").value=aramar;
+            }
+        else if((aram*12)>1320&&aram!="")
+            {
+                aramar=110*14.2+(aram-110)*15.08;
+                document.getElementById("aramar").value=aramar;
+            }
     }
 
 
 
     if(gaz<0||gaz>500&&document.getElementById("gaz").value != "")
     {
-        
         document.getElementById("hiba2").innerHTML="Hiba"
         document.getElementById("gaz").value=""
-
-    }
-    else{
-        if(gaz<149.91&&document.getElementById("gaz").value != ""){
-            gazar=gaz*85.06;
-            document.getElementById("gazar").value=gazar;
-
-        }
-        else if(gaz>149.91&&document.getElementById("gaz").value != ""){
-            
-            aramar=149.91*85.08+(gaz-149.91)*650.87;
-            document.getElementById("gazar").value=gazar;
-
-        }
     }
 
-
-
-
-
-
-    if(viz<0||viz>30){
-        
-        document.getElementById("hiba3").innerHTML="Hiba"
-        document.getElementById("viz").value=""
-
+    else
+    {
+        if(gaz<149.91&&document.getElementById("gaz").value != "")
+            {
+                gazar=gaz*85.06;
+                document.getElementById("gazar").value=gazar;
+            }
+        else if(gaz>149.91&&document.getElementById("gaz").value != "")
+            {
+                aramar=149.91*85.08+(gaz-149.91)*650.87;
+                document.getElementById("gazar").value=gazar;
+            }
     }
-    else{
 
-        if(viz>0&&viz<20){
-        
-            vizar=viz*218.95+179.95+viz*526.4;
-            document.getElementById("vizar").value=vizar;
-
-        }
-        else if(viz>20){
-            
-            vizar=179.95+viz*218.95+viz*526.4+viz*504.6;
-            document.getElementById("vizar").value=vizar;
-
-            
+    if(viz<0||viz>30)
+        {
+            document.getElementById("hiba3").innerHTML="Hiba"
+            document.getElementById("viz").value=""
         }
 
-
+    else
+    {
+        if(viz>0&&viz<20)
+            {
+                vizar=viz*218.95+179.95+viz*526.4;
+                document.getElementById("vizar").value=vizar;
+            }
+        else if(viz>20)
+            {
+                vizar=179.95+viz*218.95+viz*526.4+viz*504.6;
+                document.getElementById("vizar").value=vizar;
+            }
     }
 
-
-
-    if(kozos<6000||kozos>22000&&document.getElementById("koz").value != ""){
-        
-        
-        document.getElementById("hiba4").innerHTML="Hiba"
-        document.getElementById("koz").value=""
-
-    }
-    else if(kozos>=6000&&kozos<=22000){
-        kozosar=kozos
-        document.getElementById("kozar").value=kozosar;
-
-    }
-   
-
-
+    if(kozos<6000||kozos>22000&&document.getElementById("koz").value != "")
+        {
+            document.getElementById("hiba4").innerHTML="Hiba"
+            document.getElementById("koz").value=""
+        }
+    else if(kozos>=6000&&kozos<=22000)
+        {
+            kozosar=kozos
+            document.getElementById("kozar").value=kozosar;
+        }
 }
